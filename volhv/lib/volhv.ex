30454c1f83e90c19a6bd491a87bde5479eee20c1
Ilya Druzhinin <ilya.druzh@gmail.com>
2022-10-12 02:31:10 +0300

    back init

---
+++ volhv/lib/volhv.ex
@@ -0,0 +1,18 @@
+defmodule Volhv do
+  @moduledoc """
+  Documentation for `Volhv`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Volhv.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
